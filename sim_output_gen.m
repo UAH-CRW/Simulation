d3b60870676ff1c3432b4cdd52bbd64d1ba8ae2c
Daniel Corey <dc0069@space.uah.edu>
2018-09-29 14:11:58 -0500

    Added rocket angle and freestream pressure/density to output

--- sim_output_gen.m
+++ sim_output_gen.m
@@ -2,22 +2,22 @@ function ret = sim_output_gen(Excel, flightdata, forces, max, Eng, Roc)
 % Write the simulation and simulation input parameters described by
 % flightdata, forces, propinfo, max, Prop, Eng, and Roc into the current
 % active Excel sheet
-Excel.Range('A1:K1').Select();
+Excel.Range('A1:N1').Select();
 Excel.Selection.Value = {'Time (s)', 'Acceleration (m/s)', 'Velocity (m/s)',...
     'Altitude (m)', 'Mach Number', 'Total Delivered Impulse (N*s)', 'Y Acceleration (m/s^2)', 'Y Velocity (m/s',...
-    'X Acceleration (m/s^2)', 'X Velocity (m/s)', 'X Position (m)'};
+    'X Acceleration (m/s^2)', 'X Velocity (m/s)', 'X Position (m)', 'Angle (°)', 'Free-stream pressure (Pa)', 'Air density (kg/m3)'};
 [rows, ~] = size(flightdata);
-Excel.Range(sprintf('A2:K%i', rows)).Select();
+Excel.Range(sprintf('A2:N%i', rows)).Select();
 Excel.Selection.Value = num2cell(flightdata);
 
-Excel.Range('L1:N1').Select();
+Excel.Range('O1:Q1').Select();
 Excel.Selection.Value = {'Mass (kg)', 'Drag (N)', 'Thrust (N)'};
 [rows, ~] = size(forces);
-Excel.Range(sprintf('L2:N%i', rows)).Select();
+Excel.Range(sprintf('O2:Q%i', rows)).Select();
 Excel.Selection.Value = num2cell(forces);
 
 % Insert results
-begin_col = 16;
+begin_col = 19;
 [~, plusc] = insert_struct_into_excel(max, 'Maximums', Excel, [1, begin_col]);
 begin_col = begin_col + plusc + 2;
 [~, plusc] = insert_struct_into_excel(Eng, 'Engine', Excel, [1, begin_col]);
